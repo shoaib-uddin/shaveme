13485bad81a0dfae4b8e292f78cf1e495468d6f9
Xtreme Hardware <muhammad.n@ikotel.ca>
2018-02-15 09:35:50 +0500

    intermediate commit

--- Shave Me/HelpVC.swift
+++ Shave Me/HelpVC.swift
@@ -18,14 +18,24 @@ class HelpVC: MirroringViewController {
     @IBOutlet weak var imageSlideShow: ImageSlideshow!
     @IBOutlet weak var doneSkipButton: UIButton!
     
+<<<<<<< HEAD
+=======
+    @IBOutlet weak var videoView: UIView!
+    
+    
+    var avPlayer: AVPlayer!;
+    var avPlayerLayer: AVPlayerLayer!
+    
+>>>>>>> parent of aa71e95... All done leaving minor bugs behind
     // MARK: - Life Cycle Methods
     
     override func viewDidLoad() {
-        
-        
         super.viewDidLoad()
 
+<<<<<<< HEAD
         
+=======
+>>>>>>> parent of aa71e95... All done leaving minor bugs behind
         imageSlideShow.circular = false
         
         let selectedImages = AppController.sharedInstance.language == "en" ? INTRO_SCREENS_EN : INTRO_SCREENS_AR
@@ -50,10 +60,30 @@ class HelpVC: MirroringViewController {
         
     }
     
+<<<<<<< HEAD
     
     
     
     
+=======
+    @IBAction func xVideo(_ sender: UIButton) {
+        avPlayer.pause();
+        avPlayer = nil;
+        self.videoView.removeFromSuperview();
+    }
+    
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        
+        // Layout subviews manually
+        avPlayerLayer.frame = view.bounds
+        
+        
+        
+        
+    }
+>>>>>>> parent of aa71e95... All done leaving minor bugs behind
     
     
     
@@ -67,11 +97,17 @@ class HelpVC: MirroringViewController {
         super.viewWillAppear(animated)
         
         self.navigationController?.setNavigationBarHidden(true, animated: true);
+<<<<<<< HEAD
         
         let storyboard = UIStoryboard(name: "Main", bundle: Bundle.main);
         let destination = storyboard.instantiateViewController(withIdentifier: "AVPlayerVC") as! AVPlayerVC
         self.navigationController?.present(destination, animated: false, completion: nil);
         
+=======
+        avPlayerLayer.frame = videoView.bounds;
+        avPlayerLayer.transform = CATransform3DMakeRotation(CGFloat(90.0 / 180.0 * .pi), 0.0, 0.0, 1.0);
+        avPlayer.play() // Start the playback
+>>>>>>> parent of aa71e95... All done leaving minor bugs behind
     }
     
     override func viewWillDisappear(_ animated: Bool) {
@@ -86,6 +122,9 @@ class HelpVC: MirroringViewController {
         MyUserDefaults.getDefaults().set(false, forKey: MyUserDefaults.PREFS_SHOW_HELP)
         _ = self.navigationController?.popViewController(animated: false)
     }
+<<<<<<< HEAD
     
     
+=======
+>>>>>>> parent of aa71e95... All done leaving minor bugs behind
 }
